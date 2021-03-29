a7e48fa7b0a3e538902c118dbc6a05a569f1b7f8
Evgenia Latysh <gaallea@gmail.com>
2021-03-29 03:14:58 +0300

    WebDriver Singletone implemented
    page object added
    
    page object added
    
    Initial Commit

---
+++ src/test/java/webdrivertasks/test/WebDriverPastebinHomePageTest.java
@@ -0,0 +1,56 @@
+package webdrivertasks.test;
+
+import webdrivertasks.driver.WebDriverSetUp;
+import webdrivertasks.page.SavedPasteBinPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import webdrivertasks.page.PastebinHomePage;
+
+
+public class WebDriverPastebinHomePageTest {
+    SavedPasteBinPage savedPastebin;
+    WebDriver driver;
+    final String TITLE = "how to gain dominance among developers";
+    final String CODE = "git config --global user.name  \"New Sheriff in Town\"\n" +
+                        "git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\n" +
+                        "git push origin master --force";
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void browserSetup() {
+        driver = WebDriverSetUp.getDriver();
+    }
+
+    @Test
+    public void chekPastebinICanWinTask() {
+        savedPastebin = new PastebinHomePage(driver)
+                .openPage()
+                .inputTextInTextArea("Hello from WebDriver")
+                .inputTextInTitleArea("helloweb")
+                .selectExpirationTime()
+                .submitNewPaste();
+    }
+
+
+    @Test
+    public void chekPastebinBrightItOnTask() {
+        savedPastebin = new PastebinHomePage(driver)
+                .openPage()
+                .inputTextInTextArea(CODE)
+                .selectSyntaxHighlighting()
+                .selectExpirationTime()
+                .inputTextInTitleArea(TITLE)
+                .submitNewPaste();
+        Assert.assertTrue(savedPastebin.checkHeaderToTitleAccordance(TITLE));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void quitBrowser() {
+        driver.quit();
+        driver = null;
+    }
+
+}
